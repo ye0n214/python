23c3fba4ad3b811a2dfeeccff9d24b8aba8df2e9
ye0n214 <siyeon070214@gmail.com>
2026-03-17 21:45:13 +0900

    한글로 변경

--- CircleCal.py
+++ CircleCal.py
@@ -1,6 +1,6 @@
 pi=3.14
 radius=float(input("원의 반지름 입력>" ))
-circumference=2*pi*radius
-area=pi*radius**2
-print("circumference=", circumference)
-print("area=", area)
+원의둘레=2*pi*radius
+원의넓이=pi*radius**2
+print("원의둘레=", 원의둘레)
+print("원의넓이=", 원의넓이)
